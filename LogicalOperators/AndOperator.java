b6b67a0649251519bedbbb93acfa54ee5291f2f7
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-12-15 21:35:23 -0700

    Learn about logical AND operators:

--- LogicalOperators/AndOperator.java
+++ LogicalOperators/AndOperator.java
@@ -8,11 +8,15 @@ public class AndOperator {
         if (isAlien == true) {
             System.out.println("Sure!");
             System.out.println("How do I win?");
-            System.out.println("Get a highscore of 100!");
+            System.out.println("Get a score higher than 90!");
         }
-        int topScore = 100;
+        int topScore = 90;
         if (topScore <= 100) {
             System.out.println("You got the high score!");
+        }
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
